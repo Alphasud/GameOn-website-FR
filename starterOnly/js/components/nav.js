91151b4fc82460667ccd49975cc5aaa9a569837c
charlie <kislapsky@gmail.com>
2021-02-11 19:59:31 +0100

    Started fixing some css issues

--- starterOnly/js/components/nav.js
+++ starterOnly/js/components/nav.js
@@ -1,8 +1,15 @@
+const icon = document.querySelector('.icon');
+
+icon.addEventListener('click', function (event) {
+  event.stopPropagation();
+  editNav();
+});
+
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  var myTopNav = document.getElementById("myTopnav");
+  if (myTopNav.className === "topnav") {
+    myTopNav.className += " responsive";
   } else {
-    x.className = "topnav";
+    myTopNav.className = "topnav";
   }
 }
