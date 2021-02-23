f102b87e3064aa48e17f6f3fb9619ffeed4eea2b
charlie <kislapsky@gmail.com>
2021-02-23 07:21:35 +0100

    Reunion of all JS in one file

--- js/main.js
+++
@@ -1,6 +0,0 @@
-
-import { } from "../js/components/modal.js";
-import { } from "../js/components/nav.js";
-import { } from "../js/components/form.js";
-import { } from "./components/validators.js";
-
